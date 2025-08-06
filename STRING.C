b793a88b4563e1df7bb4bed5937b327fb0908472
Harshad0804 <harshadkarguwal@gmail.com>
2025-08-06 23:46:10 +0530

    06-August

---
+++ STRING.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+
+void main()
+{
+       char s1[50];
+       int l1;
+       clrscr();
+       printf("\nEnter String 1 : ");
+       gets(s1);
+       l1=strlen(s1);
+       printf("\nLength Of String 1 is %d",l1);
+       getch();
+
+}
