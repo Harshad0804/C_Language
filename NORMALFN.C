305943c4401bc3ed84533a93201132b9554967e3
Harshad0804 <harshadkarguwal@gmail.com>
2025-08-11 18:59:35 +0530

    11-August

---
+++ NORMALFN.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+void printLine()
+{
+	int i;
+	for(i=0;i<40;i++)
+	{
+		printf("*");
+	}
+}
+void main()
+{
+       clrscr();
+       printLine();
+       getch();
+
+}
